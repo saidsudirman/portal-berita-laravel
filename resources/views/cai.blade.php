5a0e0bdbefe80dd5ee02fc902d6a129e4d36fbd9
saidsudirman <saidsudirman17@gmail.com>
2025-01-21 20:16:19 +0800

    first commit

---
+++ resources/views/cai.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/cai">cai</a>
+    <a href="/home">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    <h1>Hello Word</h1>
+</body>
+</html>
