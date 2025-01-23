01c43aedcddf5dc9a8554f3433ea6c96c1a9e22e
saidsudirman <saidsudirman17@gmail.com>
2025-01-24 04:35:39 +0800

    sampai video 10

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/home', function () {
     return view('home', ['title' => 'Home Page']);
@@ -21,45 +11,11 @@ Route::get('/cai', function () {
     return view('cai', ['title' => 'Cai', 'nama' => 'PI + PI = PIPI']);
 });
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'blog', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'Judul-undipa-1',
-            'title' => 'UNDIPA TODAY 1',
-            'author' => 'Said',
-            'body' => 'Penerimaan Mahasiswa Baru 2025: Universitas Dipa Makassar telah membuka pendaftaran bagi calon mahasiswa baru untuk gelombang pertama tahun 2025. Manfaat yang ditawarkan antara lain bebas biaya formulir pendaftaran dan kesempatan merasakan pengalaman virtual reality. '
-        ],
-        [
-            'id' => 2,
-            'slug' => 'Judul-undipa-2',
-            'title' => 'UNDIPA TODAY 2',
-            'author' => 'Caiii',
-            'body' => 'Pemutusan Hubungan Kerja Staf Terlibat Pelecehan Seksual: Universitas Dipa Makassar mengambil tindakan tegas dengan memecat seorang staf yang terlibat dalam kasus pelecehan seksual terhadap seorang alumni. Kejadian ini terekam oleh CCTV di perpustakaan universitas'
-        ]
-    ]]);
+    return view('posts', ['title' => 'blog', 'posts' => Post::all()]);
 });
 
-Route::get('/posts/{slug}', function($slug){
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'Judul-undipa-1',
-            'title' => 'UNDIPA TODAY 1',
-            'author' => 'Said',
-            'body' => 'Penerimaan Mahasiswa Baru 2025: Universitas Dipa Makassar telah membuka pendaftaran bagi calon mahasiswa baru untuk gelombang pertama tahun 2025. Manfaat yang ditawarkan antara lain bebas biaya formulir pendaftaran dan kesempatan merasakan pengalaman virtual reality. '
-        ],
-        [
-            'id' => 2,
-            'slug' => 'Judul-undipa-2',
-            'title' => 'UNDIPA TODAY 2',
-            'author' => 'Caiii',
-            'body' => 'Pemutusan Hubungan Kerja Staf Terlibat Pelecehan Seksual: Universitas Dipa Makassar mengambil tindakan tegas dengan memecat seorang staf yang terlibat dalam kasus pelecehan seksual terhadap seorang alumni. Kejadian ini terekam oleh CCTV di perpustakaan universitas'
-        ]
-    ];
+Route::get('/posts/{post:slug}', function(Post $post){
 
-    $post = Arr ::first($posts, function ($post) use ($slug){
-        return $post['slug'] == $slug;
-    });
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 
