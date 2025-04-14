3421683c9e3da76bf4d3a32408a1a3dcfcee2c73
saidsudirman <saidsudirman17@gmail.com>
2025-04-15 00:24:06 +0800

    perbaiki admin

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\BeritaController;
 use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/home', function () {
@@ -18,7 +18,7 @@ Route::get('/home', function () {
 Route::get('/cai', function () {
     return view('cai', ['title' => 'Cai', 'nama' => 'SAID SUDIRMAN']);
 });
-Route::get('/posts', function () {
+Route::get('/', function () {
     return view('posts', ['title' => 'Blog', 'posts' =>Post::filter(request(['search', 'category', 'author']))->latest()->paginate(9)->withQueryString()]);
 });
 
@@ -51,9 +51,9 @@ Route::get('/index', function () {
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/berita', [BeritaController::class, 'index'])->name('berita');
+Route::get('/berita/create', [BeritaController::class, 'create'])->name('berita.create');
 
-Route::get('/user', [UserController::class, 'index'])->name('user');
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+Route::get('/admin/create', [AdminController::class, 'create'])->name('admin.create');
 
 Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori');
-
-Route::get('/index', [DashboardController::class, 'index'])->name('events.index');
